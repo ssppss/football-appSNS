4781cdbfaaab63f429f190527f550927d0ec19e3
sunpianpian9 <sunpianpian9@163.com>
2016-08-16 15:05:41 +0800

    add blue

--- src/scripts/utils/util.js
+++ src/scripts/utils/util.js
@@ -1,6 +1,9 @@
 var util={
 	setFocus:function(el){
 		el.addClass("active").siblings().removeClass("active");
+	},
+	render:function(){
+		console.log("blue")
 	}
 }
 //使用module.exports这个方法将util暴露出去
