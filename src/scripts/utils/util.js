bdaadf0d2b7a9ec6222c24bf83686154798d07a1
sunpianpian9 <sunpianpian9@163.com>
2016-08-16 15:03:34 +0800

    add red

--- src/scripts/utils/util.js
+++ src/scripts/utils/util.js
@@ -1,6 +1,9 @@
 var util={
 	setFocus:function(el){
 		el.addClass("active").siblings().removeClass("active");
+	},
+	fordatamat:function(){
+		console.log("red")
 	}
 }
 //使用module.exports这个方法将util暴露出去
